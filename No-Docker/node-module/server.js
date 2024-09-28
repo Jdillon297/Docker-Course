14537649d0e1bfa35c80293484b14c8e9052f6b0
Jdillon.Dev <jacobdillon.dev@gmail.com>
2024-09-28 16:27:13 -0500

    added allow orgin access to the localhost:3000

--- No-Docker/node-module/server.js
+++ No-Docker/node-module/server.js
@@ -2,13 +2,26 @@ const { PostgresClient } = require("./postgresClient");
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  var whitelist = ["localhost:5000", "localhost:3000"];
+  var host = req.get("host");
+
+  whitelist.forEach(function (val, key) {
+    if (host.indexOf(val) > -1) {
+      res.setHeader("Access-Control-Allow-Origin", host);
+    }
+  });
+
+  next();
+});
+
 app.get("/", async (req, res) => {
   const client = new PostgresClient();
   const response = await client.SelectAllQuery();
   res.json(response);
 });
 
-const port = 3000;
+const port = 8765;
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
