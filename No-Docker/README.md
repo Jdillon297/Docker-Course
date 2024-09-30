7e53952914cf9cb38ead5f489ec87b9d653b281a
Jdillon.Dev <jacobdillon.dev@gmail.com>
2024-09-29 23:52:25 -0500

    Full Docker network set up on all images
    Was able to set up dockerized react client along with nginx reverse proxy to forward requests to the docker containers.

--- No-Docker/README.md
+++ No-Docker/README.md
@@ -1,3 +1,19 @@
-This project has become pretty cool if I say so myself
+This project has become pretty cool if I say so myself.
+
+To be formal you should look at th docker-compose.yaml file.
+
+Create a .env file with those environment variables so that docker knows what is going on.
 
 run docker-compose up --build
+
+There are 2 apis running so you should navigate in the browser to:
+
+http://localhost:3000/node-api for the node.js api endpoint
+http://localhost:5050/python-api for the python api endpoint
+
+There is 1 Database running which is the postgres container. 
+
+To test that it was working go into that running contaier and use the following commands
+
+psql -U <DB_USER> -d <DB_NAME>
+
--- No-Docker/react-module/web-client/src/App.js
+++ No-Docker/react-module/web-client/src/App.js
@@ -1,26 +1,17 @@
 import DataFetcher from "./Components/DataFetcher";
 import { apiRoutes } from "./Networking/apiRoutes";
 import { baseUrl } from "./Networking/baseUrl";
-import { Ports } from "./Networking/ports";
 import { styles } from "./Styles/styles";
 function App() {
   return (
     <div style={styles.dataContainer}>
       <div>
         <h3>DATA FROM PYTHON API</h3>
-        <DataFetcher
-          url={baseUrl.url}
-          port={Ports.pythonPort}
-          route={apiRoutes.python_api}
-        />
+        <DataFetcher url={baseUrl.url} route={apiRoutes.python_api} />
       </div>
       <div>
         <h3>DATA FROM NODE API</h3>
-        <DataFetcher
-          url={baseUrl.url}
-          port={Ports.nodePort}
-          route={apiRoutes.node_api}
-        />
+        <DataFetcher url={baseUrl.url} route={apiRoutes.node_api} />
       </div>
     </div>
   );
